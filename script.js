a042f459319cd11828fdd2115acc56e2c8259d19
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 02:26:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function startGame() {
-  alert("Игра начинается!");
+  const appDiv = document.querySelector('.app');
+  appDiv.innerHTML = `
+    <h2>Уровень 1</h2>
+    <p>Вопрос: Что такое блокчейн?</p>
+    <button onclick="alert('Правильно!')">Цепочка блоков с данными</button>
+    <button onclick="alert('Неправильно')">Обычный сайт</button>
+  `;
 }
