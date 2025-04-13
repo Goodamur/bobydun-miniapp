71161d222cf122bed38c20b4cc9498e3d4c24c74
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 03:26:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function startGame() {
-  const appDiv = document.querySelector('.app');
-  appDiv.innerHTML = `
-    <h2>Уровень 1</h2>
-    <p>Вопрос: Что такое блокчейн?</p>
-    <button onclick="alert('Правильно!')">Цепочка блоков с данными</button>
-    <button onclick="alert('Неправильно')">Обычный сайт</button>
-  `;
+
+function startLesson() {
+  alert("Переход к урокам будет здесь!");
 }
