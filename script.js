2939f49cfbf553ed53b48a25c55cc255c9c6378b
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 03:51:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.text = "Поехали";
+tg.MainButton.show();
+tg.MainButton.onClick(() => {
+  startGame();
+});
 
-function startApp() {
-  const tg = window.Telegram.WebApp;
-  tg.MainButton.text = "Продолжить";
-  tg.MainButton.show();
-  tg.MainButton.onClick(() => {
-    tg.close(); // можно заменить на переход к следующему экрану
-  });
+const text = `Привет! Рад видеть тебя здесь.
+
+Меня зовут BobyDun, но можно просто Boby😉 Я тут для того, чтобы быть твоим помощником и гидом в мире криптовалют.
+Со мной ты научишься безопасно инвестировать и торговать😎
+
+Тыцни кнопку "Поехали" ⬇️, если готов, и погнали😁`;
+
+let index = 0;
+const speed = 40;
+const animatedText = document.getElementById("animatedText");
+
+function typeWriter() {
+  if (index < text.length) {
+    animatedText.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = () => {
+  typeWriter();
+};
+
+function startGame() {
+  alert("Запускаем курс! 🚀 (сюда пойдет логика следующего шага)");
 }
