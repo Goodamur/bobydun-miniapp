1b65494fa299296a0d258634beff4008e8452051
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 05:21:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+
 const tg = window.Telegram.WebApp;
 tg.expand();
-tg.MainButton.text = "Продолжить";
-tg.MainButton.show();
 
-window.onload = function () {
+window.onload = function() {
   setTimeout(() => {
-    document.getElementById("splash").classList.add("hidden");
-    document.querySelector(".app").classList.remove("hidden");
-    typeText("Привет! Рад видеть тебя здесь. Меня зовут BobyDun, но можно просто Boby. Я тут для того, что бы быть твоим помощником и гидом в мире криптовалют. Со мной ты научишься, безопасно, инвестировать и торговать. Тыцни кнопку 'Поехали', если готов, и погнали!!");
-  }, 5000);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("app").style.display = "flex";
+    document.getElementById("bg-music").play();
+    animateText("Привет! Рад видеть тебя здесь. Меня зовут Boby Dun, но можно просто Boby. Я тут для того, чтобы быть твоим помощником и гидом в мире криптовалют. Со мной ты научишься, безопасно, инвестировать и торговать. Тыцни кнопку 'Поехали', если готов, и погнали!!");
+  }, 4000);
 };
 
-function typeText(text) {
-  const textElem = document.getElementById("animatedText");
-  let index = 0;
-  const interval = setInterval(() => {
-    textElem.textContent += text[index];
-    index++;
-    if (index === text.length) clearInterval(interval);
-  }, 40);
+function animateText(text) {
+  let element = document.getElementById("animatedText");
+  element.innerHTML = '';
+  let i = 0;
+  let timer = setInterval(() => {
+    element.innerHTML += text.charAt(i);
+    i++;
+    if (i >= text.length) clearInterval(timer);
+  }, 50);
 }
 
 function startGame() {
-  tg.MainButton.hide();
-  alert("Тестовая сцена запускается...");
+  animateText("КРУТО! Ты молодец💥 Рад, что ты готов к исследованиям. А сейчас я задам тебе пару вопросов, чтобы понять твой уровень. Ты, главное, отвечай честно, для того, чтобы я мог подготовить для тебя правильную программу. Всего будет несколько тестовых вопросов, которые я придумал для тебя. Расслабься и не бойся ошибаться.");
+  document.querySelector("button").innerText = "Пройти Тест";
+  document.querySelector("button").onclick = () => window.location.href = 'test.html';
 }
