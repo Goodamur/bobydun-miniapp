a042681ef1d6805d596413f02645391560c4d5cb
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 04:53:32 +0300

    Add files via upload
    Add stage 2: welcome screen with CryBoby

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-
 const tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.text = "Продолжить";
 tg.MainButton.show();
-tg.MainButton.onClick(() => {
-  document.querySelector('.app').style.display = "none";
-  document.getElementById('question-screen').style.display = "block";
-});
 
-let fullText = `Привет! Рад видеть тебя здесь.
-Меня зовут Boby Dun, но можно просто Boby.
-Я тут для того, что бы быть твоим помощником и гидом в мире криптовалют.
-Со мной ты научишься, безопасно, инвестировать и торговать.
-Тыцни кнопку "Поехали", если готов, и погнали!!`;
-let index = 0;
-
-function typeText() {
-  const animatedText = document.getElementById("animatedText");
-  if (index < fullText.length) {
-    animatedText.textContent += fullText.charAt(index);
-    index++;
-    setTimeout(typeText, 40);
-  }
-}
-
-window.onload = function() {
+window.onload = function () {
   setTimeout(() => {
-    document.getElementById("loading-screen").style.display = "none";
-    document.querySelector(".app").style.display = "block";
-    typeText();
-  }, 4000);
+    document.getElementById("splash").classList.add("hidden");
+    document.querySelector(".app").classList.remove("hidden");
+    typeText("Привет! Рад видеть тебя здесь. Меня зовут BobyDun, но можно просто Boby. Я тут для того, что бы быть твоим помощником и гидом в мире криптовалют. Со мной ты научишься, безопасно, инвестировать и торговать. Тыцни кнопку 'Поехали', если готов, и погнали!!");
+  }, 5000);
 };
 
-function showInfo() {
-  document.querySelector(".app").style.display = "none";
-  document.getElementById("info-screen").style.display = "block";
-  setTimeout(() => {
-    document.getElementById("info-screen").style.display = "none";
-    document.getElementById("question-screen").style.display = "block";
-  }, 4000);
+function typeText(text) {
+  const textElem = document.getElementById("animatedText");
+  let index = 0;
+  const interval = setInterval(() => {
+    textElem.textContent += text[index];
+    index++;
+    if (index === text.length) clearInterval(interval);
+  }, 40);
+}
+
+function startGame() {
+  tg.MainButton.hide();
+  alert("Тестовая сцена запускается...");
 }
