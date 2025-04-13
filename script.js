bde4120003bf4d83e453c0d80bcae03db6e3087b
Goodamur <a.kaverin1993@gmail.com>
2025-04-13 04:47:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 
-function typeText(elementId, text, callback) {
-  const el = document.getElementById(elementId);
-  let i = 0;
-  function type() {
-    if (i < text.length) {
-      el.textContent += text.charAt(i);
-      i++;
-      setTimeout(type, 40);
-    } else if (callback) {
-      callback();
-    }
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.text = "Продолжить";
+tg.MainButton.show();
+tg.MainButton.onClick(() => {
+  document.querySelector('.app').style.display = "none";
+  document.getElementById('question-screen').style.display = "block";
+});
+
+let fullText = `Привет! Рад видеть тебя здесь.
+Меня зовут Boby Dun, но можно просто Boby.
+Я тут для того, что бы быть твоим помощником и гидом в мире криптовалют.
+Со мной ты научишься, безопасно, инвестировать и торговать.
+Тыцни кнопку "Поехали", если готов, и погнали!!`;
+let index = 0;
+
+function typeText() {
+  const animatedText = document.getElementById("animatedText");
+  if (index < fullText.length) {
+    animatedText.textContent += fullText.charAt(index);
+    index++;
+    setTimeout(typeText, 40);
   }
-  type();
 }
 
-window.onload = () => {
+window.onload = function() {
   setTimeout(() => {
-    document.getElementById("loader").classList.add("hidden");
-    document.getElementById("app").classList.remove("hidden");
-    typeText("animatedText", "Привет! Рад видеть тебя здесь.\nМеня зовут Boby Dun, но можно просто Boby.\nЯ тут для того, что бы быть твоим помощником и гидом в мире криптовалют.\nСо мной ты научишься, безопасно, инвестировать и торговать\nТыцни кнопку \"Поехали\", если готов, и погнали!!");
+    document.getElementById("loading-screen").style.display = "none";
+    document.querySelector(".app").style.display = "block";
+    typeText();
   }, 4000);
 };
 
-function nextStep() {
-  document.getElementById("app").classList.add("hidden");
-  document.getElementById("infoScreen").classList.remove("hidden");
+function showInfo() {
+  document.querySelector(".app").style.display = "none";
+  document.getElementById("info-screen").style.display = "block";
   setTimeout(() => {
-    document.getElementById("infoScreen").classList.add("hidden");
-    document.getElementById("dialog2").classList.remove("hidden");
-    typeText("animatedText2", "КРУТО! Ты молодец💥\nРад, что ты готов к исследованиям.\nА сейчас я задам тебе пару вопросов, чтобы понять твои уровень.\nТы, главное, отвечай честно, для того, что бы я мог подготовить, для тебя, правильную программу.\nВсего будет несколько тестовых вопросов, которые я придумал для тебя.\nРасслабься и не бойся ошибаться.");
+    document.getElementById("info-screen").style.display = "none";
+    document.getElementById("question-screen").style.display = "block";
   }, 4000);
-}
-
-function startTest() {
-  alert("Здесь будет переход к первому тесту");
 }
