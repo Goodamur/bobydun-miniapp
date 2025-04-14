962648dea2b0c53ad7028629395fb9d0e713f536
Goodamur <a.kaverin1993@gmail.com>
2025-04-14 03:10:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 
 const messages = [
-  "Привет! Рад видеть тебя здесь.",
-  "Меня зовут BobyDun, но можно просто Boby 😉",
-  "Я тут для того, чтобы быть твоим помощником и гидом в мире криптовалют.",
-  "Со мной ты научишься безопасно инвестировать и торговать 😎",
-  "Тыцни кнопку «Поехали», если готов — и погнали 🚀"
+  "Привет!\nРад видеть тебя здесь.",
+  "Меня зовут\nBobyDun.\nМожно просто Boby 😉",
+  "Я буду твоим гидом\nв мире криптовалют.",
+  "Со мной ты научишься\nне терять деньги, а зарабатывать 😎",
+  "Тыцни кнопку\n«Поехали», если готов — и\nпогнали 🚀"
 ];
 
 let index = 0;
 const textElement = document.getElementById("animatedText");
 const nextButton = document.getElementById("nextButton");
-const bubble = document.getElementById("dialog-bubble");
 
-function showText(text, callback) {
+function showText(text, showButton = false) {
   textElement.innerText = text;
-  if (callback) callback();
+  nextButton.style.display = showButton ? "block" : "none";
 }
 
 function nextStage() {
   index++;
   if (index < messages.length) {
-    showText(messages[index]);
-    if (index === messages.length - 1) {
+    const isLast = index === messages.length - 1;
+    showText(messages[index], isLast);
+    if (isLast) {
       nextButton.innerText = "Пройти тест";
       nextButton.onclick = () => window.location.href = "test.html";
     }
@@ -35,5 +35,7 @@ window.onload = function () {
     document.getElementById("loading-screen").style.display = "none";
     document.getElementById("app").style.display = "flex";
     showText(messages[index]);
-  }, 3000); // загрузка 3 секунды
+    nextButton.style.display = "block";
+    nextButton.onclick = nextStage;
+  }, 3000);
 };
